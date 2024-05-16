e62788f7aa863b901357848f66c024387262dcbb
DPhngNam <22520908@gm.uit.edu.vn>
2024-05-16 16:03:21 +0700

    make it better13 again

--- src/main/java/org/example/demobackend/Models/quan.java
+++ src/main/java/org/example/demobackend/Models/quan.java
@@ -1,11 +1,14 @@
 package org.example.demobackend.Models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public  class quan {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int maquan;
     private String tenquan;
 
