7043f6466894cc35c52f122746027eb6ec633662
DPhngNam <22520908@gm.uit.edu.vn>
2024-05-18 15:53:46 +0700

    make it better13 agains

--- src/main/java/org/example/demobackend/Controller/DVTController.java
+++ src/main/java/org/example/demobackend/Controller/DVTController.java
@@ -4,10 +4,7 @@ import org.example.demobackend.Models.dvt;
 import org.example.demobackend.Services.DVTService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/dvt")
@@ -22,5 +19,10 @@ public class DVTController {
     @PostMapping("/addDVT")
     public ResponseEntity<String> addDVT(@RequestBody dvt dvt) {
         return dvtService.insertDVT(dvt);
+    }
+
+    @GetMapping("/getALLDVT")
+    public Iterable<dvt> getAllDVT() {
+        return dvtService.getAllDVT();
     }
 }
--- src/main/java/org/example/demobackend/Services/DVTService.java
+++ src/main/java/org/example/demobackend/Services/DVTService.java
@@ -27,4 +27,8 @@ public class DVTService {
                     + e.getMessage());
         }
     }
+
+    public Iterable<dvt> getAllDVT() {
+        return dvtRepository.findAll();
+    }
 }
