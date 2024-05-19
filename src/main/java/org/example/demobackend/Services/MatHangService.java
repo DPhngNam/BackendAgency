4526f2c33545cbc9bfbe7293d968286f670900ec
DPhngNam <22520908@gm.uit.edu.vn>
2024-05-19 16:41:15 +0700

    make it better13 agains

--- src/main/java/org/example/demobackend/Services/MatHangService.java
+++ src/main/java/org/example/demobackend/Services/MatHangService.java
@@ -1,6 +1,8 @@
 package org.example.demobackend.Services;
 
+import org.example.demobackend.Models.dvt;
 import org.example.demobackend.Models.mathang;
+import org.example.demobackend.Repository.DVTRepository;
 import org.example.demobackend.Repository.MatHangRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,10 +12,13 @@ import java.util.List;
 @Service
 public class MatHangService {
     private static MatHangRepository matHangRepository;
+    private static DVTRepository dvtRepository;
 
     @Autowired
-    public MatHangService(MatHangRepository matHangRepository) {
+    public MatHangService(MatHangRepository matHangRepository, DVTRepository dvtRepository) {
+
         this.matHangRepository = matHangRepository;
+        this.dvtRepository = dvtRepository;
     }
 
     public static mathang getMatHangById(int mamh) {
@@ -29,6 +34,11 @@ public class MatHangService {
     }
 
     public static boolean createMatHang(mathang newMatHang) {
+        dvt existingDVT = dvtRepository.findByTendvt(newMatHang.getDvt().getTendvt());
+        if (existingDVT == null) {
+            return false;
+        }
+        newMatHang.setDvt(existingDVT);
         matHangRepository.save(newMatHang);
         return true;
     }
