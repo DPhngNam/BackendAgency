ce65656dab100322df40c8f3cd085e87c4979f26
DPhngNam <22520908@gm.uit.edu.vn>
2024-05-20 21:47:34 +0700

    make it better15

--- src/main/java/org/example/demobackend/Services/LoaiDaiLyService.java
+++ src/main/java/org/example/demobackend/Services/LoaiDaiLyService.java
@@ -5,14 +5,19 @@ import org.example.demobackend.Repository.LoaiDaiLyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class LoaiDaiLyService {
-    private final LoaiDaiLyRepository loaiDaiLyRepository;
+    private static LoaiDaiLyRepository loaiDaiLyRepository;
     @Autowired
     public LoaiDaiLyService(LoaiDaiLyRepository loaiDaiLyRepository) {
         this.loaiDaiLyRepository = loaiDaiLyRepository;
     }
-    public Iterable<loaidaily> getLoaiDaiLyRepository() {
-        return loaiDaiLyRepository.findAll();
+
+
+
+    public static List<loaidaily> getLoaiDaiLyRepository() {
+        return (List<loaidaily>) loaiDaiLyRepository.findAll();
     }
 }
