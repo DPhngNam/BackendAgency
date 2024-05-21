5c54a824a9777419da671f33f70ebece583cc590
Terry <138951865+Terry-UIT@users.noreply.github.com>
2024-05-22 01:14:10 +0700

    Update BCDSService

--- src/main/java/org/example/demobackend/Services/BaoCaoDoanhSoService.java
+++ src/main/java/org/example/demobackend/Services/BaoCaoDoanhSoService.java
@@ -58,7 +58,7 @@ public class BaoCaoDoanhSoService {
     }
 
     public static int createBaoCaoDoanhSo(baocaodoanhso newBaoCaoDoanhSo) {
-        try{
+        try {
             baocaodoanhso savedBaoCaoDoanhSo = baoCaoDoanhSoRepository.save(newBaoCaoDoanhSo);
             return savedBaoCaoDoanhSo.getMabaocaods();
         } catch (Exception e) {
@@ -67,53 +67,3 @@ public class BaoCaoDoanhSoService {
         }
     }
 }
-
-    /*
-        public List<ctbcds>  (thang, nam)
-
-            Repo baocaodoanhso save (new bcds) what is this?
-
-
-            
-            1 ct : tong tien tri gia cua Dai ly A theo thang nam  => sum1
-
-            int sum += tatca tri gia phieu xuat cua tat ca dai ly trong cung thang - nam
-            for(ct : ct){
-                ctbcds.settyle = sum1/ sum
-            }
-
-            m giải thích lại luôn đi, là làm được tới cái bước chỉnh pxh với tạo bcds lấy mã ròi á
-
-
-            thang, nam
-            daily A co n pxh => m tong tri gia
-                B
-                C
-                D
-            là tổng trị giá là tổng tiền của n cái phiếu xuất hàng của daily A trong tháng đó năm đó
-            ye hiểu
-
-            thang, nam
-            mA + mB +... = sumTong
-
-            ctbaocaodanhso
-            daily , sophieuxuat, tong tri gia m, ty le = m/sum
-            => List
-
-            daily nạp từ đâu vào z
-            là nạp vào đại lý, đếm số phiếu xuất của đại lý đó trong tháng đó năm đó
-            tính 2 cái tổng, 1 cái là tổng trị giá của đại lý đó trong tháng đó năm đó, 1 cái là tổng tr giá của tổng các đại lý
-            ròi chia ra tỷ lệ
-            z hả
-            đếm spx là getSoPhieuXuatByMaDailyAndThangAndNam
-            nạp cái mã đại lý giống bên postman khi nãy
-            getTongTienPhieuXuatByMaDailyAndThangAndNam là tổng 1
-            getTongTienPhieuXuatByThangAndNam là tổng 2
-            ròi chia tỷ lệ
-            nguyên cái mớ này tính toán trong cái ctbcds hả ngay cái chỗ create
-            oke để t mò trước ròi t push lên, mai sai thì sửa
-
-
-
-
-     */
