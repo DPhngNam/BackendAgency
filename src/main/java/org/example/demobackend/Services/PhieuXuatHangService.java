98901f2acb1115baaa16c5f35e85adc543d423ae
DPhngNam <22520908@gm.uit.edu.vn>
2024-05-22 15:54:26 +0700

    make it better

--- src/main/java/org/example/demobackend/Services/PhieuXuatHangService.java
+++ src/main/java/org/example/demobackend/Services/PhieuXuatHangService.java
@@ -31,13 +31,14 @@ public class PhieuXuatHangService {
 
     public static int createPhieuXuatHang(phieuxuathang newPhieuXuatHang) {
         try {
-            int tienno_moi = newPhieuXuatHang.getTongtien() - newPhieuXuatHang.getSotientra();
-            if(tienno_moi > 0){
-                dailyService.updateSoNo(newPhieuXuatHang.getMadaily().getMadaily(), tienno_moi);
-                phieuXuatHangRepository.save(newPhieuXuatHang);
-            }
+//            int tienno_moi = newPhieuXuatHang.getTongtien() - newPhieuXuatHang.getSotientra();
+//            if(tienno_moi >= 0){
+//                dailyService.updateSoNo(newPhieuXuatHang.getMadaily().getMadaily(), tienno_moi);
+//            }
+            phieuXuatHangRepository.save(newPhieuXuatHang);
             return newPhieuXuatHang.getMapxuat();
         } catch (Exception e) {
+            e.printStackTrace();
             return -1;
         }
     }
