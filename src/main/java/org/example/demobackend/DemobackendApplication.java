5cbcfc1eeaedecda59a7ad0d9a14153e06fea18a
DPhngNam <22520908@gm.uit.edu.vn>
2024-06-08 01:22:09 +0700

    feat: auto create db in local host

--- src/main/java/org/example/demobackend/DemobackendApplication.java
+++ src/main/java/org/example/demobackend/DemobackendApplication.java
@@ -5,13 +5,36 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
 public class DemobackendApplication {
 
+    private static final String DB_URL = "jdbc:sqlserver://localhost:1433";
+    private static final String USER = "sa";
+    private static final String PASS = "password";
+    private static final String DB_NAME = "Retailer";
+
     public static void main(String[] args) {
+        initializeDatabase();
         SpringApplication.run(DemobackendApplication.class, args);
     }
 
+    private static void initializeDatabase() {
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+             Statement stmt = conn.createStatement()) {
+
+            String sql = "IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = '" + DB_NAME + "') " +
+                    "CREATE DATABASE " + DB_NAME;
+            stmt.execute(sql);
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
